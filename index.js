51ba95d3c0af0de78ff3db9ea386dc63ca2f63b3
Ethan Obott <217574989+ECC06@users.noreply.github.com>
2026-09-12 11:59:05 +0000

    works now

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
 
 const app = express();
