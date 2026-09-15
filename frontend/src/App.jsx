9ba096fb9992db1321f578f0ba574b37ec36c7fa
Ethan Obott <217574989+ECC06@users.noreply.github.com>
2026-09-15 21:20:38 +0000

    removed replace()

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,7 @@ import { LoginPage, RegisterPage } from "./components/AuthPage";
 import BrowsePage from "./components/BrowsePage";
 import Navigation from "./components/Navigation";
 
-const configuredApiUrl = import.meta.env.API_URL;
-
-const API_URL = configuredApiUrl.replace(/\/+$/, "").replace(/\/books$/, "");
+const API_URL = import.meta.env.API_URL;
 
 const pages = ["browse", "add", "register", "login"];
 
